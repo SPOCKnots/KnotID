59b95a743d77ed686d7b83054a4002b0b1397204
Alexander Taylor <alexanderjohntaylor@gmail.com>
2015-02-21 00:05:39 +0000

    Fixed torus knot and message flashing on failure

--- analysis.py
+++ analysis.py
@@ -2,6 +2,7 @@
 import numpy as n
 import json
 from pyknot2.spacecurves import Knot
+from pyknot2.make.torus import knot as torus_knot
 
 
 
@@ -16,11 +17,11 @@ def text_to_json(text):
             raise ValueError('Invalid text passed.')
         points.append([float(v) for v in values])
 
-    k = Knot(points)
+    k = Knot(points, verbose=False)
     return knot_to_json(k)
         
 
-def knot_to_json(knot):
+def knot_to_json(k):
     k.zero_centroid()
     max_extent = n.max(n.max(n.abs(k.points), axis=0))
     identification = k.identify()
@@ -28,14 +29,24 @@ def knot_to_json(knot):
     extra_stuff = {'identification': identification,
                    'gauss_code': str(k.gauss_code()),
                    'alex_roots': k.alexander_at_root((2, 3, 4)),
-                   'hyp_vol': str(k.hyperbolic_volume()),
+                   # 'hyp_vol': str(k.hyperbolic_volume()),
                    'vassiliev_degree_2': k.vassiliev_degree_2()}
 
-    gc = k.gauss_code()
-    if len(gc) < 100:
-        extra_stuff['alexander'] = str(
-            k.alexander_polynomial(mode='cypari')).replace('*', '')
+    # try:
+    #     gc = k.gauss_code()
+    #     if len(gc) < 100:
+    #         extra_stuff['alexander'] = str(
+    #             k.alexander_polynomial(mode='cypari')).replace('*', '')
 
-        print(extra_stuff['alexander'])
+    #         print(extra_stuff['alexander'])
+    # except ValueError, RuntimeError:
+    #     print('ValueError in alexander calculation, only a problem '
+    #           'if running in debug mode.')
+    
     
     return (json.dumps(k.points.tolist()), 2.5*max_extent, extra_stuff)
+
+
+def torus_knot_to_json(p, q):
+    
+    return knot_to_json(Knot(torus_knot(p, q, 300)*10, verbose=False))
--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-from flask import Flask, url_for, render_template, request
-from analysis import text_to_json
+from __future__ import print_function
+from flask import (Flask, url_for, render_template, request, redirect,
+                   flash)
+from analysis import text_to_json, torus_knot_to_json
+from fractions import gcd
 
 app = Flask(__name__)
 
+with open('secret_key.bin', 'rb') as fileh:
+    app.secret_key = fileh.read()
 
 @app.route('/')
 def main():
@@ -10,46 +15,89 @@ def main():
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
+
     if request.method == 'POST':
-        if 'pointstext' in request.form:
-            pointstext = request.form['pointstext']
-            text = pointstext
-            lines = pointstext.split('\n')
+        return upload_post()
 
-        if 'pointsfile' in request.files:
-            pointsfile = request.files['pointsfile']
-            text = pointsfile.read().decode('utf8')
-            lines = text.split('\n')
+    elif request.method == 'GET':
+        return upload_get()
 
-        num_lines = len(lines)
+    return render_template('upload_fail.html')
 
-        parse_success = False
+def upload_get():
+    args = request.args
+
+    if args['mode'] == 'torus':
+        if 'p' not in args or 'q' not in args:
+            flash('p and/or q parameters were not provided')
+            return upload_fail()
 
         try:
-            array_json, camera_extent, extra_stuff = text_to_json(text)
-            parse_success = True
+            p = int(args['p'])
+            q = int(args['q'])
         except ValueError:
-            print('Caught ValueError')
-            array_json = []
-            camera_extent = 5
-            extra_stuff = {}
+            flash('Could not convert p and q to integers')
+            return upload_fail()
 
-        tube_points = 10*num_lines
+        if gcd(p, q) != 1:
+            flash('Chosen p and q are not coprime')
+            return upload_fail()
 
-        return render_template('upload.html', num_lines=num_lines,
-                               parse_success=parse_success,
-                               line_points=array_json,
-                               camera_extent=camera_extent,
-                               tube_points=tube_points,
-                               **extra_stuff)
-
+        array_json, camera_extent, extra_stuff = torus_knot_to_json(p, q)
     else:
-        return render_template('upload_fail.html')
+        flash('invalid mode')
+        return upload_fail()
+    tube_points = 600
+
+    return render_template('upload.html', 
+                           parse_success=True,
+                           line_points=array_json,
+                           camera_extent=camera_extent,
+                           tube_points=tube_points,
+                           **extra_stuff)
+
+
+def upload_fail():
+    return redirect(url_for('main'))
+
+def upload_post():
+    if 'pointstext' in request.form:
+        pointstext = request.form['pointstext']
+        text = pointstext
+        lines = pointstext.split('\n')
+
+    if 'pointsfile' in request.files:
+        pointsfile = request.files['pointsfile']
+        text = pointsfile.read().decode('utf8')
+        lines = text.split('\n')
+
+    num_lines = len(lines)
+
+    parse_success = False
+
+    try:
+        array_json, camera_extent, extra_stuff = text_to_json(text)
+        parse_success = True
+    except ValueError:
+        print('Caught ValueError')
+        array_json = []
+        camera_extent = 5
+        extra_stuff = {}
+
+    tube_points = 10*num_lines
+
+    return render_template('upload.html', num_lines=num_lines,
+                           parse_success=parse_success,
+                           line_points=array_json,
+                           camera_extent=camera_extent,
+                           tube_points=tube_points,
+                           **extra_stuff)
+
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
 if __name__ == "__main__":
-    app.run()
-    #app.run(debug=True)
+    #app.run()
+    app.run(debug=True)
